54db4121b40ddd63ad24e079a5df13ca1e3750f7
Shaheer-op9872uw <shaheerminhas123@gmail.com>
2024-10-27 11:39:33 +0500

    Tuples homework

---
+++ act1.py
@@ -0,0 +1,25 @@
+numbers = (1, 2, 3, 4, 5)
+
+first = numbers[0]
+second = numbers[1]
+
+sub_numbers = numbers[1:4]
+
+extended_numbers = numbers + (6, 7)
+
+size = len(numbers)
+
+has_three = 3 in numbers
+
+two_position = numbers.index(2)
+
+threes_count = numbers.count(3)
+
+print(first)
+print(second)
+print(sub_numbers)
+print(extended_numbers)
+print(size)
+print(has_three)
+print(two_position)
+print(threes_count)
